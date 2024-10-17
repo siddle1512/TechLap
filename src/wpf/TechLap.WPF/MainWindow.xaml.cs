a78b6afe50ea08104938495bc4a8c9b04381df9e
Quan Cao Minh <quancm4603@gmail.com>
2024-10-17 23:12:05 +0700

    [Add] Unfinished Login Admin

--- src/wpf/TechLap.WPF/MainWindow.xaml.cs
+++ src/wpf/TechLap.WPF/MainWindow.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Text;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+using System.Net.Http;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TechLap.WPF
 {
@@ -19,6 +15,41 @@ namespace TechLap.WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try { 
+                var username = txtUsername.Text;
+                var password = txtPassword.Text;
+
+                // Create HttpClient 
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                var content = JsonConvert.SerializeObject(new { Username = username, Password = password });
+                StringContent data = new StringContent(content, Encoding.UTF8, "application/json");
+
+                // Send request to API
+                HttpResponseMessage response = await client.PostAsync(ConfigurationManager.AppSettings["ApiEndpoint"] + "/api/admins/login", data);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // Parse JSON response
+                var jsonResponse = JObject.Parse(responseBody);
+
+                if (jsonResponse["isSuccess"] != null)
+                {
+                    if (jsonResponse["isSuccess"].Value<bool>())
+                    {
+                        MessageBox.Show("Login successfully");
+                        GlobalState.Token = jsonResponse["data"].Value<string>();
+                    }
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
