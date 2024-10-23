c76fe158d543358d00c26908ce31d417acf892c3
siddle1512 <binhbobinhbo22@gmail.com>
2024-10-23 18:42:40 +0700

    [Add] Add-GetProductsByModel

--- src/api/TechLap.API/Controllers/ProductController.cs
+++ src/api/TechLap.API/Controllers/ProductController.cs
@@ -31,15 +31,15 @@ namespace TechLap.API.Controllers
             return CreateResponse<IEnumerable<ProductResponse>>(true, "Request processed successfully.", HttpStatusCode.OK, response);
         }
 
-        //[HttpGet]
-        //[Authorize(Roles = "User, Admin")]
-        //[Route("/api/products/")]
-        //public async Task<IActionResult> GetProducts([FromQuery] SearchProductRequestByModel request)
-        //{
-        //    var products = await _productRepository.GetAllAsync(p => p.Model.ToLower().Contains(request.Model.ToLower()));
-        //    var response = LazyMapper.Mapper.Map<IEnumerable<ProductResponse>>(products);
-        //    return CreateResponse<IEnumerable<ProductResponse>>(true, "Request processed successfully.", HttpStatusCode.OK, response);
-        //}
+        [HttpGet]
+        [Authorize(Roles = "User, Admin")]
+        [Route("/api/products/search")]
+        public async Task<IActionResult> GetProducts([FromQuery] SearchProductRequestByModel request)
+        {
+            var products = await _productRepository.GetAllAsync(p => p.Model.ToLower().Contains(request.Model.ToLower()));
+            var response = LazyMapper.Mapper.Map<IEnumerable<ProductResponse>>(products);
+            return CreateResponse<IEnumerable<ProductResponse>>(true, "Request processed successfully.", HttpStatusCode.OK, response);
+        }
 
         [HttpPost]
         [Authorize(Roles = "User, Admin")]
