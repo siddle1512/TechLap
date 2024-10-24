b6bf5d35b3886db9f1ac18dc039640d9f2a9cc7f
Quan Cao Minh <quancm4603@gmail.com>
2024-10-24 08:45:41 +0700

    [Update] Theme and Dashboard

--- src/wpf/TechLap.WPF/Dashboard.xaml.cs
+++ src/wpf/TechLap.WPF/Dashboard.xaml.cs
@@ -23,5 +23,59 @@ namespace TechLap.WPF
         {
             InitializeComponent();
         }
+
+        // Event handlers to load the respective UserControl into the ContentControl
+        private void EmployeeManagement_Click(object sender, RoutedEventArgs e)
+        {
+            //MainContentArea.Content = new EmployeeManager();  // Load Employee management UserControl
+        }
+
+        private void LaptopManagement_Click(object sender, RoutedEventArgs e)
+        {
+            //MainContentArea.Content = new LaptopManager();  // Load Laptop management UserControl
+        }
+
+        private void CategoryManagement_Click(object sender, RoutedEventArgs e)
+        {
+            //MainContentArea.Content = new CategoryManager();  // Load Category management UserControl
+        }
+
+        private void UserManagement_Click(object sender, RoutedEventArgs e)
+        {
+            //MainContentArea.Content = new UserManager();  // Load User management UserControl
+        }
+
+        private void OrderManagement_Click(object sender, RoutedEventArgs e)
+        {
+            //MainContentArea.Content = new OrderManager();  // Load Order management UserControl
+        }
+
+        private void SearchLaptop_Click(object sender, RoutedEventArgs e)
+        {
+            //MainContentArea.Content = new LaptopSearch();  // Load Laptop search UserControl
+        }
+
+        private void PromotionManagement_Click(object sender, RoutedEventArgs e)
+        {
+            //MainContentArea.Content = new PromotionManager();  // Load Promotion management UserControl
+        }
+
+        private void GroupChat_Click(object sender, RoutedEventArgs e)
+        {
+            //MainContentArea.Content = new GroupChatControl();  // Load Group Chat UserControl
+        }
+
+        private void CustomerManager_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentArea.Content = new CustomerManager();
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            GlobalState.Clear();
+            LoginWindow login = new LoginWindow();
+            login.Show();
+            this.Close();
+        }
     }
 }
